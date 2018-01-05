fdd1fc0020763f87b462e1042317f9ede9db7e0c
juan <theaxford@gmail.com>
2018-01-05 12:30:09 +0100

    change gearStack definition

--- src/core/GearStack.js
+++ src/core/GearStack.js
@@ -1,8 +1,12 @@
 export default class GearStack{
-  constructor(parent, stack = []){
-    this.stack = new Array(stack.length)
-    for (let i = 0; i < stack.length; i++) {
-      this.stack[i] = stack[i].gear
+  constructor(parent, stack = {}){
+    const gears = Object.entries(stack)
+    this.stack = new Array(gears.length)
+    let i = 0
+    for (let [key, value] of gears){
+      parent[key] = value
+      this.stack[i] = value
+      i++
     }
     this.pause = false
     this.active = true
